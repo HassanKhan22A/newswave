238237670c900769e3a1cbc55c0e9dd50e88d0ff
HassanKhan22A <97021635+HassanKhan22A@users.noreply.github.com>
2023-11-25 16:12:34 +0530

    Update App.js

--- newswave/src/App.js
+++ newswave/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        This project is under construction
+        <NavBar/>
+        <News/>
       </div>
     )
   }
